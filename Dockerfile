66025482436370b25cd16ba3b4e8a6a4093dd1ff
Peter McConnell <me@petermcconnell.com>
2016-04-14 21:58:59 +0100

    Moved docker commands to shell script. Added SSL unsafe option for interacting with the kubernetes api

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,11 @@ FROM nginx:stable-alpine
 MAINTAINER Peter McConnell <peter.mcconnell@rehabstudio.com>
 
 # add scooby proxy generator
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
 ADD ./scooby_proxy_alpine /scooby_proxy_alpine
 ADD config.json /config.json
 RUN chmod +x /scooby_proxy_alpine
-RUN echo "*/5 * * * * /scooby_proxy_alpine" >> /etc/crontabs/root
+RUN echo "*/1 * * * * cd / && ./scooby_proxy_alpine" >> /etc/crontabs/root
+
+CMD /start.sh
--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"log"
-	"os"
-	"net/http"
-	"os/exec"
-	"fmt"
-	"encoding/json"
-	"io/ioutil"
-	"text/template"
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"text/template"
 )
 
 type ApiResponsePods struct {
@@ -18,12 +18,13 @@ type ApiResponsePods struct {
 
 type ApiResponsePodItems struct {
 	Metadata ApiResponsePodItemMetadata `json:"metadata"`
-	Spec     ApiResponsePodItemSpec `json:"spec"`
+	Spec     ApiResponsePodItemSpec     `json:"spec"`
 }
 
 type ApiResponsePodItemMetadata struct {
-	Name string `json:"name"`
-	Labels ApiResponsePodItemMetadataLabel `json:"labels"`
+	Name         string                          `json:"name"`
+	GenerateName string                          `json:"generateName"`
+	Labels       ApiResponsePodItemMetadataLabel `json:"labels"`
 }
 
 type ApiResponsePodItemMetadataLabel struct {
@@ -35,8 +36,8 @@ type ApiResponsePodItemSpec struct {
 }
 
 type ApiResponsePodItemSpecContainers struct {
-	Name string `json:"name"`
-	Image string `json:"image"`
+	Name  string                                 `json:"name"`
+	Image string                                 `json:"image"`
 	Ports []ApiResponsePodItemSpecContainerPorts `json:"ports"`
 }
 
@@ -45,19 +46,19 @@ type ApiResponsePodItemSpecContainerPorts struct {
 }
 
 type NginxConf struct {
-	Resolver         string
-	Subdomain        string
-	Tld              string
-	Port             int
-	HtpasswdEnabled  bool
+	Resolver        string
+	Subdomain       string
+	Tld             string
+	Port            int
+	HtpasswdEnabled bool
 }
 
 type Config struct {
-	Resolver string `json:"resolver"`
-	Tld string `json:"tld"`
-	HtpasswdEnabled bool `json:"htpasswdenabled"`
+	Resolver        string `json:"resolver"`
+	Tld             string `json:"tld"`
+	HtpasswdEnabled bool   `json:"htpasswdenabled"`
 	DefaultHtpasswd string `json:"defaulthtpasswd"`
-	Namespace string `json:"namespace"`
+	Namespace       string `json:"namespace"`
 }
 
 func main() {
@@ -65,20 +66,33 @@ func main() {
 	url := "config.json"
 	file, e := ioutil.ReadFile(url)
 	if e != nil {
-        fmt.Printf("File error: %v\n", e)
-        os.Exit(1)
-    }
-    var config Config
-    json.Unmarshal(file, &config)
-    
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	var config Config
+	json.Unmarshal(file, &config)
 
+	// remove current files
+	cleanUpConfd()
+
+	// generate configs
 	generateConfigsFromKubernetesAPI(config)
 
 	// reload nginx
 	cmd := "nginx"
 	args := []string{"-s", "reload"}
 	if err := exec.Command(cmd, args...).Run(); err != nil {
-		os.Exit(1)
+		log.Fatal(err)
+	}
+}
+
+func cleanUpConfd() {
+	dir := "/etc/nginx/conf.d/"
+	if err := os.RemoveAll(dir); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.Mkdir(dir, 0755); err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -88,37 +102,41 @@ func generateConfigsFromKubernetesAPI(config Config) {
 		log.Fatal(err)
 	}
 	tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    client := &http.Client{Transport: tr}
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
 	url := "https://" + os.Getenv("KUBERNETES_SERVICE_HOST") + ":" + os.Getenv("KUBERNETES_PORT_443_TCP_PORT") + "/api/v1/namespaces/" + config.Namespace + "/pods"
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(token)))
 	res, err := client.Do(req)
-    if err != nil {
-    	log.Fatal(err)
-    }
-    defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
 
 	file, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-        log.Fatal(err)
-    }
-    var pods ApiResponsePods
-    json.Unmarshal(file, &pods)
-    for i := 0; i < len(pods.Items); i++ {
-    	createNginxConfig(config, pods.Items[i].Metadata.Labels.Name, pods.Items[i].Spec.Containers[0].Ports[0].ContainerPort)
-    }
+		log.Fatal(err)
+	}
+	var pods ApiResponsePods
+	json.Unmarshal(file, &pods)
+	for i := 0; i < len(pods.Items); i++ {
+		if pods.Items[i].Metadata.Name == os.Getenv("HOSTNAME") {
+			// don't add ourself
+			continue
+		}
+		createNginxConfig(config, subdomain, pods.Items[i].Spec.Containers[0].Ports[0].ContainerPort)
+	}
 }
 
 func createNginxConfig(config Config, subdomain string, port int) {
 	htpasswd := config.DefaultHtpasswd
 	conf := NginxConf{
-		Resolver:         config.Resolver,
-		Tld:              config.Tld,
-		HtpasswdEnabled:  config.HtpasswdEnabled,
-		Subdomain:        subdomain,
-		Port:             port,
+		Resolver:        config.Resolver,
+		Tld:             config.Tld,
+		HtpasswdEnabled: config.HtpasswdEnabled,
+		Subdomain:       subdomain,
+		Port:            port,
 	}
 	nginxTemplate, err := template.New("nginxconf").Parse(`server {
     listen       80;
@@ -142,7 +160,7 @@ func createNginxConfig(config Config, subdomain string, port int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Create("/etc/nginx/conf.d/" + subdomain)
+	f, err := os.Create("/etc/nginx/conf.d/" + subdomain + ".conf")
 	if err != nil {
 		log.Println("create file: ", err)
 		return
